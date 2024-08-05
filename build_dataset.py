ac2655c885092612a603a717fcd36f9581c5b8dc
Marian Lippold <marian.lippold@gmx.de>
2024-08-05 13:40:14 +0200

    idk

--- build_dataset.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset, Audio, Dataset
-
-# cv-corpus: Liste mit Audiodateien für den Merged-Datensatz zusammenstellen
-# Schritt 1: Anforderungen klären
-# Möglichst Ausgewogene Altersverteilung
--- miscellaneous.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-torch.cuda.is_available()
-torch.cuda.device_count()
-torch.cuda.current_device()
-torch.cuda.get_device_name()
