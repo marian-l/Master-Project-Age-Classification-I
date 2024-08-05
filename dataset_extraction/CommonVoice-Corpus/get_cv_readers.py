3bfbbf38c648d21c15eb401d6e7ba9398f4f01d8
Marian Lippold <marian.lippold@gmx.de>
2024-08-05 13:40:02 +0200

    update trainset getter

--- dataset_extraction/CommonVoice-Corpus/get_cv_readers.py
+++ dataset_extraction/CommonVoice-Corpus/get_cv_readers.py
@@ -17,6 +17,8 @@ path_to_test = PATH + TEST
 AGES = ["teens", "twenties", "thirties", "fourties", "fifties", "sixties", "seventies", "eighties"]
 GENDERS = ["male_masculine", "female_feminine"]
 
+_ages = ["<20", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89"]
+
 
 def get_gender_age_distribution_chart(read_path: str, write_path: str = ""):
     df = pd.read_csv(filepath_or_buffer=read_path, encoding="utf-8", header=0)
@@ -24,12 +26,15 @@ def get_gender_age_distribution_chart(read_path: str, write_path: str = ""):
     gender_distribution = []
     for gender in GENDERS:
         temp_df = df[df["gender"] == gender]
-        gender_distribution.append([gender, temp_df.size])
+        gender_distribution.append([gender, temp_df.shape[0]])
 
     age_distribution = []
+
+    temp = 0
     for age in AGES:
         temp_df = df[df["age"] == age]
-        age_distribution.append([age, temp_df.size])
+        age_distribution.append([_ages[temp], temp_df.shape[0]])
+        temp += 1
 
     gender_count = 0
     age_count = 0
@@ -45,7 +50,7 @@ def get_gender_age_distribution_chart(read_path: str, write_path: str = ""):
     age_distribution = dict(age_distribution)
     gender_distribution = dict(gender_distribution)
 
-    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.figsize"] = [11, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
     fig, axs = plt.subplots(1, 2)
@@ -65,32 +70,32 @@ def dataset_quality_filter(read_path: str, write_path: str = "", sep: str = None
     # filter AGE: "age" and "nan"
     filtered = df["age"].isin(AGES)
     df = df[filtered]
-    df.to_csv(header=df.columns, path_or_buf='../data' + write_path + '/CV-FilteredAges.csv', mode="w")
+    df.to_csv(header=df.columns, path_or_buf='../_datasets' + write_path + '/CV-FilteredAges.csv', mode="w")
 
     filtered = df['gender'].isin(GENDERS)
     df = df[filtered]
-    df.to_csv(header=df.columns, path_or_buf='../data' + write_path + '/CV-FilteredAgesAndGenders.csv', mode="w")
+    df.to_csv(header=df.columns, path_or_buf='../_datasets' + write_path + '/CV-FilteredAgesAndGenders.csv', mode="w")
 
     # filter gender: "male_masculine" and "female_feminine"
     filtered = df['gender'].isin(['male_masculine'])
     male_df = df[filtered]
-    male_df.to_csv(header=male_df.columns, path_or_buf='../data' + write_path + '/male_CV-FilteredAgesAndGenders.csv', mode="a")
+    male_df.to_csv(header=male_df.columns, path_or_buf='../_datasets' + write_path + '/male_CV-FilteredAgesAndGenders.csv', mode="a")
 
     filtered = df['gender'].isin(['female_feminine'])
     female_df = df[filtered]
-    female_df.to_csv(header=female_df.columns, path_or_buf='../data' + write_path + '/female_CV-FilteredAgesAndGenders.csv', mode="a")
+    female_df.to_csv(header=female_df.columns, path_or_buf='../_datasets' + write_path + '/female_CV-FilteredAgesAndGenders.csv', mode="a")
 
     # free memory
     del df
 
     # filter duplicate entries to get total amount of unique speakers per gender
     male_df.drop_duplicates(keep='first', inplace=True, subset='client_id')
-    male_df.to_csv(header=male_df.columns, path_or_buf='../data' + write_path + '/male_CV-FilteredAges-UniqueSpeakers.csv', mode="a")
+    male_df.to_csv(header=male_df.columns, path_or_buf='../_datasets' + write_path + '/male_CV-FilteredAges-UniqueSpeakers.csv', mode="a")
 
     del male_df
 
     female_df.drop_duplicates(keep='first', inplace=True, subset='client_id')
-    female_df.to_csv(header=female_df.columns, path_or_buf='../data' + write_path + '/female_CV-FilteredAges-UniqueSpeakers.csv',
+    female_df.to_csv(header=female_df.columns, path_or_buf='../_datasets' + write_path + '/female_CV-FilteredAges-UniqueSpeakers.csv',
                      mode="a")
 
     del female_df
@@ -141,7 +146,7 @@ def get_each_speakers_clips(read_path: str, write_path: str = "", sep: str = "")
     else:
         current_info = f"{parts[3]}-{parts[4]}"
     # df.to_csv('../data/CV-AllSpeakers-CorrespondingClips.csv')
-    df.to_csv('../data' + write_path + 'ClipsPerDataset/CV-' + current_info + '-CorrespondingClips.csv')
+    df.to_csv('../_datasets' + write_path + 'ClipsPerDataset/CV-' + current_info + '-CorrespondingClips.csv')
 
 
 def get_train_datasets(read_path: str, write_path: str = ""):
@@ -172,37 +177,54 @@ def get_train_datasets(read_path: str, write_path: str = ""):
             current_train_set.append(modified_group)
 
         current_train_set = pd.concat(current_train_set, ignore_index=True)
-        current_train_set.to_csv(path_or_buf='../data' + write_path + 'Trainsets/CV-' + gender + '-' + str(drop) + '-Trainset.csv', header=True, index=True)
+        current_train_set.to_csv(path_or_buf='../_datasets' + write_path + 'Trainsets/CV-' + gender + '-' + str(drop) + '-Trainset.csv', header=True, index=True)
 
     # iterate over different amounts of rows to drop
     for drop in drop_count:
         current_train_set = []
+        remainders_train_set = []
+
         # drop rows and append modified to current_train_set
         for age, group in grouped:
+            deleted_entries = group
             try:
                 modified_group = group.drop(group.index[drop:group.size])
+                deleted_entries = deleted_entries.drop(deleted_entries.index[0:drop-1])
+
             except IndexError:
                 modified_group = group.drop(group.index[0:group.size])
+
             current_train_set.append(modified_group)
+            remainders_train_set.append(deleted_entries)
+
         current_train_set = pd.concat(current_train_set, ignore_index=True)
-        current_train_set.to_csv(path_or_buf='../data' + write_path + 'Trainsets/CV-' + gender + '-' + str(drop) + '-Remaining-Trainset.csv', header=True, index=True)
+        remainders_train_set = pd.concat(remainders_train_set, ignore_index=True)
+        current_train_set.to_csv(path_or_buf='../_datasets' + write_path + 'Trainsets/CV-' + gender + '-' + str(drop) + '-Remaining-Trainset.csv', header=True, index=True)
+        remainders_train_set.to_csv(path_or_buf='../_datasets' + write_path + 'Trainsets/CV-' + gender + '-' + str(drop) + '-Remaining-Trainset.csv', header=True, index=True)
 
-def do_all(read_path: str):
-    write_path = '\\Train.tsv\\'
-    # dataset_quality_filter(read_path, write_path=write_path, sep='\t')
 
-    # reads the gender filtered datasets
-    # get_train_datasets("../data/Train.tsv/male_CV-FilteredAgesAndGenders.csv", write_path=write_path)
-    # get_train_datasets("../data/Train.tsv/female_CV-FilteredAgesAndGenders.csv", write_path=write_path)
+def get_union_dfs_val_train():
+    df_1 = pd.read_csv("../_datasets/CommonVoice/Validated.tsv/male_CV-FilteredAges-UniqueSpeakers.csv")
+    df_2 = pd.read_csv("../_datasets/CommonVoice/Train.tsv/male_CV-FilteredAges-UniqueSpeakers.csv")
 
-    # reads the original dataset
-    get_each_speakers_clips(read_path, write_path=write_path, sep='\t')
+    unique_male_speakers_union_df = pd.concat([df_1, df_2]).drop_duplicates().reset_index(drop=True)
 
-    # reads the Unique Speakers Lists
-    get_gender_age_distribution_chart("../data/Train.tsv/male_CV-FilteredAges-UniqueSpeakers.csv", write_path=write_path)
-    get_gender_age_distribution_chart("../data/Train.tsv/female_CV-FilteredAges-UniqueSpeakers.csv", write_path=write_path)
+    df_1 = pd.read_csv("../_datasets/CommonVoice/Validated.tsv/female_CV-FilteredAges-UniqueSpeakers.csv")
+    df_2 = pd.read_csv("../_datasets/CommonVoice/Train.tsv/female_CV-FilteredAges-UniqueSpeakers.csv")
 
-do_all("D:\\Sprachdaten\\cv-corpus-17.0-2024-03-15-de\\cv-corpus-17.0-2024-03-15\\de\\train.tsv")
+    unique_female_speakers_union_df = pd.concat([df_1, df_2]).drop_duplicates().reset_index(drop=True)
+
+    unique_female_speakers_union_df.to_csv(path_or_buf="../_datasets/CommonVoice/Union_Train-Validated/unique_female_speakers.csv",
+                                           encoding="UTF-8", header=True)
+    unique_male_speakers_union_df.to_csv(path_or_buf="../_datasets/CommonVoice/Union_Train-Validated/unique_male_speakers.csv",
+                                         encoding="UTF-8", header=True)
+
+# get_union_dfs_val_train()
+
+# get_gender_age_distribution_chart("../_datasets/CommonVoice/Union_Train-Validated/unique_male_speakers.csv")
+# get_gender_age_distribution_chart("../_datasets/CommonVoice/Union_Train-Validated/unique_female_speakers.csv")
+
+# do_all("D:\\Sprachdaten\\cv-corpus-17.0-2024-03-15-de\\cv-corpus-17.0-2024-03-15\\de\\train.tsv")
 
 # fix_encoding()
 # get_speakers(path, "../data/Validated-Common-Voice-Speakers.csv")
@@ -217,8 +239,9 @@ do_all("D:\\Sprachdaten\\cv-corpus-17.0-2024-03-15-de\\cv-corpus-17.0-2024-03-15
 
 # get the gender and age distribution charts for male and female
 # get_gender_age_distribution_chart("../data/Validated-Common-Voice-Speakers.csv")
-# get_gender_age_distribution_chart("../data/male_CV-FilteredAges-UniqueSpeakers.csv")
-# get_gender_age_distribution_chart("../data/female_CV-FilteredAges-UniqueSpeakers.csv")
+# get_gender_age_distribution_chart("../data/Validated.tsv/male_CV-FilteredAges-UniqueSpeakers.csv")
+# get_gender_age_distribution_chart("../data/Validated.tsv/female_CV-FilteredAges-UniqueSpeakers.csv")
+
 # print("")
 
 # get_train_datasets("../data/male_CV-FilteredAges-UniqueSpeakers.csv")
@@ -230,8 +253,10 @@ import glob
 # for file in trainsets:
 #     get_each_speakers_clips(file)
 
-
-
 # df = pd.read_csv(filepath_or_buffer='../data/Trainsets/CV-female-50-Trainset.csv')
 # df = pd.read_csv(filepath_or_buffer='../data/Trainsets/CV-male-50-Trainset.csv')
 # df = pd.read_csv(filepath_or_buffer='../data/Trainsets/CV-male-50-Trainset.csv')
+
+
+##### alle zu einem datensatz gelöschten Speaker in einem separaten Dataset
+##### alle zu einem datensatz gehörigen Audioclips + Alterskategorien
